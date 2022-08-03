3712d6b9255a7d65a5c4ea79ee790bb1673ddff2
Filipe Garrote <filipe.garrote@gmail.com>
2022-08-03 17:40:38 -0300

    v3.0 - feat(rotas): adicionada navegação entre as páginas de login e de cadastro

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,17 +1,41 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/big-logo.svg"
 import Container from "../common/Container";
 import { Form, SignUpNavigation } from "../common/Form";
 
 export default function SignUp(){
+
+  const [signUpData, setSignUpData] = useState({
+    email: "",
+    name: "",
+    image: "",
+    password: "",
+  })
+
+  function handleForm(e){
+    setSignUpData({
+      ...signUpData,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  function sendForm(e){
+    e.preventDefault();
+    console.log(signUpData)
+
+
+  }
+
+
   return (
     <Container>
       <img src={logo} alt="logo"/>
-      <Form>
-        <input type="email" placeholder="email"/>
-        <input type="password" placeholder="senha"/>
-        <input type="text" placeholder="nome"/>
-        <input type="url" placeholder="foto"/>
+      <Form onSubmit={sendForm}>
+        <input type="email" placeholder="email" name="email" onChange={handleForm}/>
+        <input type="password" placeholder="senha" name="password" onChange={handleForm}/>
+        <input type="text" placeholder="nome" name="name" onChange={handleForm}/>
+        <input type="url" placeholder="foto" name="image" onChange={handleForm}/>
         <button>Entrar</button>
       </Form>
       <SignUpNavigation>
