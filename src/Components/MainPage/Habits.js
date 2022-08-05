c86871f3fef51d86f25c043c6a8f1d33d18f7ca1
Filipe Garrote <filipe.garrote@gmail.com>
2022-08-04 23:27:35 -0300

    v13.0 - feat(hábito pendente): Add componente de criação de hábito (card para criar novo hábito)

--- src/Components/MainPage/Habits.js
+++ src/Components/MainPage/Habits.js
@@ -11,6 +11,7 @@ export default function Habits(){
 
     promise.then(res => {
       setHabits(res.data)
+      console.log(res.data)
     })
   }, [])
 
@@ -20,6 +21,7 @@ export default function Habits(){
         <Title>Meus hábitos</Title>
         <div>+</div>
       </TitleContainer>
+      <PendingHabit />
       {habits.length === 0 ? 
         <Comment>Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</Comment>
       :
@@ -29,10 +31,28 @@ export default function Habits(){
 
 }
 
+function PendingHabit(){
+  const week = ["Q", "S", "T", "Q", "Q", "S", "S"]
+
+  return(
+    <HabitCard>
+      <input type="text" placeholder="nome do hábito"/>
+      <WeekContainer>
+        {week.map(day => <div>{day}</div>)}
+      </WeekContainer>
+      <Buttons>
+        <span>Cancelar</span>
+        <button>Salvar</button>
+      </Buttons>
+    </HabitCard>
+  )
+}
+
 const TitleContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin-bottom: 20px;
 
   div{
     width: 40px;
@@ -58,4 +78,66 @@ const Comment = styled.span`
   color: #666666;
   display: block;
   margin-top: 30px;
+`
+
+const HabitCard = styled.div`
+  height: 180px;
+  background-color: white;
+  border-radius: 5px;
+  padding: 18px;
+
+  input{
+    width: 100%;
+    height: 45px;
+    border: 1px solid #D4D4D4;
+    font-size: 20px;
+    padding-left: 11px;
+    border-radius: 5px;
+    color: #666666;
+    margin-bottom: 8px;
+
+    &::placeholder{
+      color: #DBDBDB;
+    }
+  }
+`
+
+const WeekContainer = styled.div`
+  display: flex;
+  gap: 4px;
+  margin-bottom: 30px;
+
+  div{
+    width: 30px;
+    height: 30px;
+    border: 1px solid #D4D4D4;
+    border-radius: 5px;
+    color: #dbdbdb;
+    font-size: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+const Buttons = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 23px;
+
+  span{
+    color: #52B6FF;
+    font-size: 16px;
+  }
+
+  button{
+    width: 84px;
+    height: 35px;
+    border: none;
+    background-color: #52B6FF;
+    border-radius: 5px;
+    color: #FFFFFF;
+    font-size: 16px;
+  }
 `
