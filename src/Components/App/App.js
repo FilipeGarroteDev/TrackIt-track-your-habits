2c1a5be78ea0ccf1d03e9aebcef129b1b0254341
Filipe Garrote <filipe.garrote@gmail.com>
2022-08-08 15:00:24 -0300

    Versão Final - feat(BÔNUS PESSOAL - Theme Switcher): Adicionado botão para alterar o tema do app, no canto inferior direito das telas.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeContext, ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 import ProgressContext from "../../contexts/ProgressContext";
 import UserContext from "../../contexts/UserContext";
 import {Reset, GlobalStyle} from "../../globalStyle";
